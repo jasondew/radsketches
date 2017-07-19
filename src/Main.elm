af4ca149afb52ea50e656bcaa78976fc1b0da5c5
Jason Dew <jason.dew@gmail.com>
2017-07-18 21:45:24 -0400

    Updates

--- src/Main.elm
+++ src/Main.elm
@@ -35,10 +35,42 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ id "header" ]
+    div
+        [ class "container-fluid" ]
+        [ div [ class "row" ] [ headerView ]
+        , div [ class "row" ] [ artworkView, bioView, artworkView ]
+        , div [ class "row" ] [ artworkView, contactView, artworkView ]
+        , div [ class "row" ] [ artworkView, longArtworkView ]
+        ]
+
+
+headerView : Html Msg
+headerView =
+    div
+        [ id "header", class "col" ]
         [ span [ class "main" ] [ text "RADsketches" ]
         , span [ class "diminuitive" ] [ text ".com" ]
         ]
+
+
+bioView : Html Msg
+bioView =
+    div [ id "bio", class "col" ] [ text "BIO" ]
+
+
+contactView : Html Msg
+contactView =
+    div [ id "contact", class "col" ] [ text "CONTACT INFO" ]
+
+
+artworkView : Html Msg
+artworkView =
+    div [ class "artwork", class "col" ] [ text "ART" ]
+
+
+longArtworkView : Html Msg
+longArtworkView =
+    div [ class "long-artwork", class "col col-9" ] [ text "LONG ART" ]
 
 
 
