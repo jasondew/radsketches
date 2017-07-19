015b185ebc27e1af3445edd69c976ea7ffc7533e
Jason Dew <jason.dew@gmail.com>
2017-07-18 20:56:09 -0400

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,55 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div [ id "header" ]
+        [ span [ class "main" ] [ text "RADsketches" ]
+        , span [ class "diminuitive" ] [ text ".com" ]
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
